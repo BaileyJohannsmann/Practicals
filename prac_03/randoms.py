ace470c8915c7b6ae579e69c1438a44a8d06225c
BaileyJohannsmann <110573860+BaileyJohannsmann@users.noreply.github.com>
2022-10-02 17:11:25 +1000

    update randoms.py

---
+++ prac_03/randoms.py
@@ -0,0 +1,14 @@
+"""
+CP1404 - Practical 3
+randoms
+program to produce random values
+"""
+
+import random
+
+# Question 1: Smallest:5 Largest:19 I saw a 9
+# Question 2: Smallest:3 Largest:9,I saw a 9, No it could not produce a 4
+# Question 3: I saw a float number (3.858242212661559),Smallest:2.5 Largest is 5.49..
+
+
+random_number = random.randrange(1, 100+1)
