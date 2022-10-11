8f4fc01f589268fb3094e84e870838254d748139
BaileyJohannsmann <110573860+BaileyJohannsmann@users.noreply.github.com>
2022-10-12 08:26:50 +1000

    Program that gets an integer from the user and does not crash when a non-number is entered

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -1,30 +1,28 @@
 
-"""
-CP1404/CP5632 Practical
-Data file -> lists program
-"""
-
 FILENAME = "subject_data.txt"
 
 
 def main():
     data = get_data()
-    print(data)
+    print_stats(data)
 
 
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
-    input_file = open(FILENAME)
-    for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
-        line = line.strip()  # Remove the \n
-        parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
-        parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
-    input_file.close()
+    data = []
+    with open(FILENAME) as input_file:
+        for line in input_file:
+            line = line.strip()
+            parts = line.split(',')
+            parts[2] = int(parts[2])
+            data.append(parts)
+    return data
+
+
+def print_stats(data):
+    """ Print the stats of classes from data"""
+    for class_info in data:
+        print(f"{class_info[0]} is taught by {class_info[1]} and has {class_info[2]} students")
 
 
 main()
