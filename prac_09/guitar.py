48201d7a25a8106b53b8603387ff43c24fc7c3ff
BaileyJohannsmann <110573860+BaileyJohannsmann@users.noreply.github.com>
2022-11-23 22:19:40 +1000

    Guitar Class

---
+++ prac_09/guitar.py
@@ -0,0 +1,24 @@
+"""
+CP1404 - Practical 9
+Guitar Class
+"""
+import datetime
+
+VINTAGE_AGE = 50
+
+
+class Guitar:
+
+    def __init__(self, name="", year=0, cost=0):
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        return f"{self.name} ({self.year}) : ${self.cost:,.2f}"
+
+    def get_age(self):
+        return datetime.date.today().year - self.year
+
+    def is_vintage(self):
+        return self.get_age() >= VINTAGE_AGE
